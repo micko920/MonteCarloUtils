5574c94bc46eb6b7737bd131af438b67bb88a027
gitlab@enviroaccounts.com <gitlab@enviroaccounts.com>
2021-08-10 17:24:22 +1200

    typops and move functions to other package

--- R/CalcMonteCarloSamles.R
+++ R/CalcMonteCarloSamles.R
@@ -7,9 +7,7 @@
 
 
 
-
-
-# @export
+#' @export
 CalcMonteCarlo <- function(title, # Name to use on debug and plots
                            est, # CO2e estimate
                            calc, # Function to calculate CO2e from inputs
@@ -22,25 +20,4 @@ CalcMonteCarlo <- function(title, # Name to use on debug and plots
   return(list(value = SamplesWithBounds(est, result), MCresults = result))
 }
 #####
-
-##### Value Models #####
-
-
-# @export
-vwuNorm <- function(v, n, ...) {
-  sd <- (diff(range(v)) / (2 * qnorm(p = QUCI))) # LCI,UCI are 90% QCI (5%,95%)
-  return(rnorm(n, mean = ValueWithUncertaintyValue(v), sd = sd))
-}
-
-# @export
-vwuTriangle <- function(v, n, ...) {
-  return(rtriangle(n = n, theta = ValueWithUncertaintyValue(v), lower = min(v), upper = max(v)))
-}
-
-# @export
-create_vwuSampled <- function(value_samples) {
-  return(function(v, n, ...) {
-    return(sample(value_samples, size = n, replace = TRUE))
-  })
-}
 
